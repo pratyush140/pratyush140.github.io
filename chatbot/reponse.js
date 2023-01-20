5d0ea2ba675e261bcb321a92b8dc2deb03541e39
pratyush140 <118610254+pratyush140@users.noreply.github.com>
2023-01-20 23:19:30 +0530

    Commands given to bot

---
+++ chatbot/reponse.js
@@ -0,0 +1,14 @@
+const responseObj = {
+  "Hello LeadBot": "Hey Pratyush ! How are you doing ?",
+  
+  hey: "Hey! What's Up",
+  
+  "show me today's date": new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+  "Whom you work for ?":"The Web Wizard Groups",
+  Creator:"Pratyush",
+  "Are you human friendly?":"Yes,I am",
+  "I am Good! How are you ?":"Nice:)Thanks for asking",
+  "Are you a robot?":"Yes I am",
+  
+};
